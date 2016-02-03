c88c2a7aea60b5ab6c612af0ae58ab8194f39a30
qibutian <603986018@qq.com>
2016-02-03 17:09:25 +0800

    first

---
+++ rabbit/src/com/means/rabbit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.means.rabbit;
+
+import com.example.rabbit.R;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
