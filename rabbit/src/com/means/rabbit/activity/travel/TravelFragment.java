47084d73728d44b2cb7ab08d0a1fff2672b5380b
qibutian <603986018@qq.com>
2016-02-07 01:02:06 +0800

    提交

--- rabbit/src/com/means/rabbit/activity/travel/TravelFragment.java
+++ rabbit/src/com/means/rabbit/activity/travel/TravelFragment.java
@@ -1,13 +1,20 @@
 package com.means.rabbit.activity.travel;
 
+import net.duohuo.dhroid.adapter.NetJSONAdapter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
+import com.means.rabbit.R;
 import com.means.rabbit.activity.my.MyIndexFragment;
+import com.means.rabbit.api.API;
+import com.means.rabbit.views.RefreshListViewAndMore;
 
 public class TravelFragment extends Fragment implements OnClickListener {
 	static TravelFragment instance;
@@ -15,6 +22,12 @@ public class TravelFragment extends Fragment implements OnClickListener {
 	View mainV;
 
 	LayoutInflater mLayoutInflater;
+
+	RefreshListViewAndMore listV;
+
+	ListView contentListV;
+
+	NetJSONAdapter adapter;
 
 	public static TravelFragment getInstance() {
 		if (instance == null) {
@@ -28,7 +41,7 @@ public class TravelFragment extends Fragment implements OnClickListener {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// mainV = inflater.inflate(R.layout.fragment_home_page, null);
+		mainV = inflater.inflate(R.layout.fragment_travel, null);
 		mLayoutInflater = inflater;
 		initView();
 		// TODO Auto-generated method stub
@@ -36,6 +49,20 @@ public class TravelFragment extends Fragment implements OnClickListener {
 	}
 
 	private void initView() {
+		listV = (RefreshListViewAndMore) mainV.findViewById(R.id.my_listview);
+		contentListV = listV.getListView();
+		adapter = new NetJSONAdapter(API.text, getActivity(),
+				R.layout.item_travel_list);
+		adapter.fromWhat("list");
+		listV.setAdapter(adapter);
+		contentListV.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1,
+					int position, long arg3) {
+
+			}
+		});
 	}
 
 	@Override
