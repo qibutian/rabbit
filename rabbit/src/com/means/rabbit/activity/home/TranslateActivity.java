8f62fcd170eb13e6f32c51b7a1cfef1d37023b75
qibutian <603986018@qq.com>
2016-02-11 00:01:39 +0800

    提交

--- rabbit/src/com/means/rabbit/activity/home/TranslateActivity.java
+++ rabbit/src/com/means/rabbit/activity/home/TranslateActivity.java
@@ -1,5 +1,21 @@
 package com.means.rabbit.activity.home;
 
-public class TranslateActivity {
+import android.os.Bundle;
+
+import com.means.rabbit.base.RabbitBaseActivity;
+
+public class TranslateActivity extends RabbitBaseActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
