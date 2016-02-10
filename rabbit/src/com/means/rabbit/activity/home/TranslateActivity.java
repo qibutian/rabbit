8d2095608f276c9d37ccfb2108b7bccec840d576
hu845643018 <845643018@qq.com>
2016-02-11 01:10:54 +0800

    更新代码

--- rabbit/src/com/means/rabbit/activity/home/TranslateActivity.java
+++ rabbit/src/com/means/rabbit/activity/home/TranslateActivity.java
@@ -2,18 +2,26 @@ package com.means.rabbit.activity.home;
 
 import android.os.Bundle;
 
+import com.means.rabbit.R;
 import com.means.rabbit.base.RabbitBaseActivity;
 
+/**
+ * 翻译助手
+ * @author dell
+ *
+ */
 public class TranslateActivity extends RabbitBaseActivity{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_translate);
 	}
 
 	@Override
 	public void initView() {
+		setTitle("文件助手");
 		// TODO Auto-generated method stub
 		
 	}
