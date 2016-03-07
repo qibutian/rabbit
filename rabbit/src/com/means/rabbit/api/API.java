7a4baaf87e17e53bb0875c48528a646397f08328
qibutian <603986018@qq.com>
2016-03-07 18:18:19 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	rabbit/src/com/means/rabbit/api/API.java

--- rabbit/src/com/means/rabbit/api/API.java
+++ rabbit/src/com/means/rabbit/api/API.java
@@ -57,5 +57,21 @@ public class API {
 
 	/** 美食列表 */
 	public static String foodList = Baseurl + "/home/shop/contentlist";
+	
+	/** 消息列表分页 */
+	public static String msglist = Baseurl + "/home/index/msglist";
+	
+	/** 消息删除 */
+	public static String msgdelete = Baseurl + "/home/index/msgdelete";
+	
+	/** 关于我们 */
+	public static String aboutdetail = Baseurl + "/home/index/aboutdetail";
+	
+	/** 修改手机 */
+	public static String changephone = Baseurl + "/home/user/changephone";
+	
+	/** 修改头像 */
+	public static String editfceimg = Baseurl + "/home/user/editfceimg";
+
 
 }
