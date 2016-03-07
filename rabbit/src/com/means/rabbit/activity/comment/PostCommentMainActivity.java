460ee6c9cd574af9ac7961b311148f2e36372998
hu845643018 <845643018@qq.com>
2016-03-07 18:14:41 +0800

    update

---
+++ rabbit/src/com/means/rabbit/activity/comment/PostCommentMainActivity.java
@@ -0,0 +1,31 @@
+package com.means.rabbit.activity.comment;
+
+import com.means.rabbit.R;
+import com.means.rabbit.R.layout;
+import com.means.rabbit.base.RabbitBaseActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * 
+ * 发布评论
+ * @author Administrator
+ *
+ */
+public class PostCommentMainActivity extends RabbitBaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_post_comment_main);
+	}
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+}
